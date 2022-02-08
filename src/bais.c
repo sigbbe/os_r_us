3306cd4cc18069393478b8261483541f2f7eb957
Sigbjørn Berdal <sigbjorn.berdal14@gmail.com>
2022-02-08 16:26:30 +0100

    updated: bais.c

--- src/bais.c
+++ src/bais.c
@@ -5,22 +5,27 @@
 #include <unistd.h>
 
 int main() {
-  char buf[128];
-  ssize_t n;
-  char *str = NULL;
-  size_t len = 0;
-  while ((n = read(STDIN_FILENO, buf, sizeof buf))) {
-    if (n < 0) {
-      if (errno == EAGAIN)
-        continue;
-      perror("read");
-      break;
-    }
-    str = realloc(str, len + n + 1);
-    memcpy(str + len, buf, n);
-    len += n;
-    str[len] = '\0';
+  //   char buf[128];
+  //   ssize_t n;
+  //   char *str = NULL;
+  //   size_t len = 0;
+  //   while ((n = read(STDIN_FILENO, buf, sizeof buf))) {
+  //     if (n < 0) {
+  //       if (errno == EAGAIN)
+  //         continue;
+  //       perror("read");
+  //       break;
+  //     }
+  //     str = realloc(str, len + n + 1);
+  //     memcpy(str + len, buf, n);
+  //     len += n;
+  //     str[len] = '\0';
+  //   }
+  //   printf("%.*s\n", len, str);
+  if (fork() == 0) {
+    printf("%s", "CHILD");
+  } else {
+    printf("%s", "PARENT");
   }
-  printf("%.*s\n", len, str);
   return 0;
 }
