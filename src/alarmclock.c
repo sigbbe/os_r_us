f9031691cb7d492510622ec949e66f690de4fcd7
Sigbjørn Berdal <sigbjorn.berdal14@gmail.com>
2022-02-28 19:02:03 +0100

    update: with args

--- src/alarmclock.c
+++ src/alarmclock.c
@@ -1,4 +1,5 @@
 #define _XOPEN_SOURCE
+#define _GNU_SOURCE
 #include "../include/my_time.h"
 #include <alloca.h>
 #include <ctype.h>
@@ -14,8 +15,60 @@
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
-
 #define NUM_ALARMS 3
+
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#define BUF_SIZE 1024
+
+// https://ofstack.com/C++/9293/linux-gets-pid-based-on-pid-process-name-and-pid-of-c.html
+void get_pid_by_name(char *task_name, int pids[], int len) {
+  DIR *dir;
+  struct dirent *ptr;
+  FILE *fp;
+  char filepath[50]; // The size is arbitrary, can hold the path of cmdline file
+  char cur_task_name[50]; // The size is arbitrary, can hold to recognize the
+                          // command line text
+  char buf[BUF_SIZE];
+  dir = opendir("/proc"); // Open the path to the
+  int i = 0;
+  int limit = 0;
+  if (NULL != dir) {
+    while ((ptr = readdir(dir)) !=
+           NULL) // Loop reads each file/folder in the path
+    {
+      // If it reads "." or ".." Skip, and skip the folder name if it is not
+      // read
+      if ((strcmp(ptr->d_name, ".") == 0) || (strcmp(ptr->d_name, "..") == 0))
+        continue;
+      if (DT_DIR != ptr->d_type)
+        continue;
+      sprintf(filepath, "/proc/%s/status",
+              ptr->d_name);      // Generates the path to the file to be read
+      fp = fopen(filepath, "r"); // Open the file
+      if (NULL != fp) {
+        if (fgets(buf, BUF_SIZE - 1, fp) == NULL) {
+          fclose(fp);
+          continue;
+        }
+        sscanf(buf, "%*s %s", cur_task_name);
+        // Print the name of the path (that is, the PID of the process) if the
+        // file content meets the requirement
+        if (!strcmp(task_name, cur_task_name)) {
+          //   printf("%s\n", cur_task_name);
+          //   printf("%d\n", i);
+          pids[i++] = atoi(ptr->d_name);
+          limit = 0;
+        }
+        fclose(fp);
+        limit++;
+      }
+    }
+    closedir(dir); // Shut down the path
+  }
+}
 
 typedef int bool;
 #define true 1
@@ -38,8 +91,65 @@ static const char *const actions[6] = {
 
 struct Alarm {
   int pid;
-  int t_time;
+  time_t t_time;
 };
+
+struct Alarm new_alarm(void) {
+  struct Alarm alarm;
+  alarm.pid = 0;
+  alarm.t_time = 0;
+  return alarm;
+}
+
+bool alarm_valid_time(struct Alarm *alarm) {
+  time_t now = unix_timestamp_now();
+  return alarm->t_time > now && now + 94608000 > alarm->t_time;
+}
+
+bool is_alarm_unset(struct Alarm *alarm) {
+  return alarm->pid == 0 && alarm->t_time == 0;
+}
+
+bool alarm_exists(struct Alarm *alarm) {
+  int status;
+  return 0 < waitpid(alarm->pid, &status, WNOHANG) && alarm_valid_time(alarm);
+}
+
+struct Alarm parse_alarm(char *line) {
+  // line: PID:<pid>,TIME:<unix timestamp>
+  char *ptr = line;
+  struct Alarm alarm = new_alarm();
+  while (*ptr) { // While there are more characters to process...
+    if (isdigit(*ptr)) {
+      // Found a number
+      long int val = (long int)strtol(ptr, &ptr, 10); // Read number
+      if (alarm.pid == 0) {
+        alarm.pid = val;
+      } else if (alarm.t_time == 0) {
+        alarm.t_time = val;
+      } else {
+        break;
+      }
+    } else {
+      // Otherwise, move on to the next character.
+      ptr++;
+    }
+  }
+  return alarm;
+}
+
+int i_round(int a, int b) {
+  return a % b >= (b / 2) ? a + b - a % b : a - a % b;
+}
+
+bool has_active_alarm(struct Alarm alarm[], int start, int end) {
+  if (start + 1 >= end) {
+    return !(alarm_valid_time(&alarm[start]) && alarm_exists(&alarm[start]));
+  } else {
+    return !(alarm_valid_time(&alarm[start]) && alarm_exists(&alarm[start])) ||
+           has_active_alarm(alarm, start + 1, end);
+  }
+}
 
 void welcome(void) {
   char w[150];
@@ -53,10 +163,6 @@ void welcome(void) {
             "\"e\"(exit), \"h\"(help), \"r\"(clear)\n");
   printf("%s\n", w);
   return;
-}
-
-int i_round(int a, int b) {
-  return a % b >= (b / 2) ? a + b - a % b : a - a % b;
 }
 
 time_t schedule_alarm_menu() {
@@ -78,6 +184,15 @@ void watch_alarm(int time) {
   execvp(args[0], args);
   fprintf(stderr, "\nChild process could play alarm sound!\n");
   exit(1);
+}
+
+void schedule_from_alarm(struct Alarm *alarm) {
+  int pid = fork();
+  if (pid != 0) {
+    alarm->pid = pid;
+  } else {
+    watch_alarm(alarm->t_time);
+  }
 }
 
 void schedule(struct Alarm alarm[], int num_alarms, long int new_alarm_time) {
@@ -102,25 +217,6 @@ void schedule(struct Alarm alarm[], int num_alarms, long int new_alarm_time) {
   }
   printf("%s\n", "Cannot create more alarms");
 }
-struct Alarm new_alarm(void) {
-  struct Alarm alarm;
-  alarm.pid = 0;
-  alarm.t_time = 0;
-  return alarm;
-}
-
-bool is_alarm_unset(struct Alarm *alarm) {
-  return alarm->pid == 0 && alarm->t_time == 0;
-}
-
-bool has_active_alarm(struct Alarm alarm[], int start, int end) {
-  if (start + 1 >= end) {
-    return !is_alarm_unset(&alarm[start]);
-  } else {
-    return !is_alarm_unset(&alarm[start]) ||
-           has_active_alarm(alarm, start + 1, end);
-  }
-}
 
 void list(struct Alarm alarm[], int len) {
   if (!has_active_alarm(alarm, 0, len)) {
@@ -130,7 +226,7 @@ void list(struct Alarm alarm[], int len) {
   printf("PID\t\tAlarm\t\t\t\tID\n");
   printf("_________________________________________________\n");
   for (int i = 0; i < len; i++) {
-    if (!is_alarm_unset(&alarm[i])) {
+    if (alarm_valid_time(&alarm[i])) {
       int time = alarm[i].t_time;
       int *time_copy = malloc(sizeof &time);
       *time_copy = time;
@@ -169,7 +265,7 @@ int cancel_alarm_menu(struct Alarm alarm[], int len) {
   return 0;
 }
 
-int main(int argc, char **argv) {
+int interactive() {
   struct Alarm alarms[NUM_ALARMS];
   // initialize unset alarms
   for (int i = 0; i < NUM_ALARMS; i++) {
@@ -214,6 +310,128 @@ int main(int argc, char **argv) {
     }
   }
   return 0;
+}
+
+int read_alarms(char *filename, struct Alarm *items, int k) {
+  FILE *fileptr;
+  char *buffer;
+  long filelen;
+  const char separator[3] = "\r\n";
+  char *line = NULL;
+  int i = 0;
+
+  for (int j = 0; j < k; j++) {
+    items[j] = new_alarm();
+  }
+
+  fileptr = fopen(filename, "rb"); // Open the file in binary mode
+  fseek(fileptr, 0, SEEK_END);     // Jump to the end of the file
+  filelen = ftell(fileptr);        // Get the current byte offset in the file
+  rewind(fileptr);                 // Jump back to the beginning of the file
+
+  buffer = (char *)malloc(filelen * sizeof(char)); // Enough memory for the file
+  fread(buffer, filelen, 1, fileptr);              // Read in the entire file
+  // -------------------------------
+  line = strtok(buffer, separator);
+  while (line != NULL && i < k) {
+    struct Alarm tmp_alarm = parse_alarm(line);
+    if (alarm_valid_time(&tmp_alarm)) {
+      items[i] = tmp_alarm;
+      if (alarm_exists(&items[i]) == 0) {
+        schedule_from_alarm(&items[i]);
+      }
+    }
+    i++;
+    line = strtok(NULL, separator); // NULL means continue from where the
+  }
+  // -------------------------------
+  fclose(fileptr); // Close the file
+  return 0;
+}
+
+int write_alarms(struct Alarm *alarms, int n) {
+  //   FILE *pFile;
+  //   char *yourFilePath = "./alarmclock.txt";
+  //   char *yourBuffer = malloc(100);
+
+  //   for (int i = 0; i < n; i++) {
+  //     char *alarm_str;
+  //     sprintf(alarm_str, "PID:%d,TIME:%ld", alarms[i].pid,
+  //     alarms[i].t_time); strcat(yourBuffer, alarm_str);
+  //   }
+
+  //   int yorBufferSize = strlen(yourBuffer) + 1;
+
+  //   /* Reserve memory for your readed buffer */
+  //   char *readedBuffer = malloc(yorBufferSize);
+
+  //   if (readedBuffer == 0) {
+  //     puts("Can't reserve memory for Test!");
+  //   }
+
+  //   /* Write your buffer to disk. */
+  //   pFile = fopen(yourFilePath, "wb");
+  //   if (pFile) {
+  //     fwrite(yourBuffer, yorBufferSize, 1, pFile);
+  //   }
+  //   return fclose(pFile);
+  return 0;
+}
+
+void kill_alarms(int argc, char **argv) {
+  int *pids = malloc(sizeof(int) * 10);
+  char *program = malloc(16);
+  strcpy(program, argv[0]);
+  program += 2;
+  get_pid_by_name(program, pids, 10);
+  for (int i = 0; i < 10; i++) {
+    if (pids[i] != 0 && pids[i] != getpid()) {
+      kill(pids[i], SIGKILL);
+    }
+  }
+}
+
+int with_args(int argc, char **argv) {
+  kill_alarms(argc, argv);
+  struct Alarm alarms[NUM_ALARMS];
+  int tmp_alarms = read_alarms("./alarmclock.txt", alarms, NUM_ALARMS);
+  if (0 == strcmp(&argv[1][0], actions[SCHEDULE]) && argc == 4) {
+    char time_str[20];
+    strcpy(time_str, argv[2]);
+    strcat(time_str, " ");
+    strcat(time_str, argv[3]);
+    time_t new_alarm_time = str_to_unix_timestamp_seconds(time_str);
+    schedule(alarms, NUM_ALARMS, new_alarm_time);
+    return 0;
+  }
+  if (argc != 2) {
+    printf("Type a valid character, type 'h' for help.\n");
+    return 0;
+  }
+  if (0 == strcmp(&argv[1][0], actions[LIST])) {
+    list(alarms, NUM_ALARMS);
+  } else if (0 == strcmp(&argv[1][0], actions[CANCEL])) {
+    list(alarms, NUM_ALARMS);
+    cancel_alarm_menu(alarms, NUM_ALARMS);
+  } else if (0 == strcmp(&argv[1][0], actions[HELP])) {
+    welcome();
+  } else if (0 == strcmp(&argv[1][0], actions[CLEAR])) {
+    system("clear");
+  } else if (0 == strcmp(&argv[1][0], actions[EXIT])) {
+    printf("\nBYE :)\n");
+  } else {
+    printf("Type a valid character, type 'h' for help.\n");
+  }
+  return write_alarms(alarms, NUM_ALARMS);
+}
+
+int main(int argc, char **argv) {
+  if (argc == 1) {
+    return interactive();
+  } else if (argc >= 2) {
+    return with_args(argc, argv);
+  }
+  return 1;
 }
 
 // gcc -std=gnu99 -o main src/lib.c src/alarmclock.c
